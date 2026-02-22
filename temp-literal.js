21d3a4f6747b04b1caf98e9f5b02b0e4c52c1685
vamsy <helen123@gmail.com>
2026-02-22 21:59:52 +0530

    Template literal example

---
+++ temp-literal.js
@@ -0,0 +1,9 @@
+// Template literal example
+let first_name = "Vamsi";
+let full_name = `Hi,${first_name} Kumar`;
+console.log(full_name)
+
+
+// Sample math
+let math = `2+2=${2 + 2}`;
+console.log(math)
