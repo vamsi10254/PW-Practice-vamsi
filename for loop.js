6fa5a0ab534986e4a9128561fd307dae73c21e76
vamsy <helen123@gmail.com>
2026-03-03 01:18:54 +0530

    for loop exer

---
+++ for loop.js
@@ -0,0 +1,28 @@
+// 1
+for (let i = 1; i <= 3; i++) {
+    console.log(i);
+}
+
+// 2
+for (let i = 1; i < 3; i++) {
+    console.log(i);
+}
+
+// 3
+for (let i = 3; i > 0; i--) {
+    console.log(i);
+}
+
+// 4
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+}
+
+console.log(i);
+
+// 5
+for (var i = 0; i < 3; i++) {
+    console.log(i);
+}
+
+console.log(i);
