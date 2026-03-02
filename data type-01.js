5d360f6056da8accbf7eb6da739a4d81991b2188
vamsy <helen123@gmail.com>
2026-03-02 23:29:33 +0530

    Data types

--- data type-01.js
+++ data type-01.js
@@ -11,3 +11,17 @@ console.log(typeof isVisible);
 
 console.log(typeof undefined); // "undefined"
 console.log(typeof null);      // "object" - What is the type of null? -typeof null returns "object" due to a historical bug in JavaScript.
+
+console.log(5 == "5");   // true
+console.log(5 === "5");  // false
+
+let countFromUI = "5";   // UI returns string
+let expectedCount = 5;   // number
+
+if (countFromUI == expectedCount) {
+    console.log("Pass");
+}
+
+console.log(false == 0);
+console.log(false === 0);
+
