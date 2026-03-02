5137493de1b6b97cdf9d24babc632ab7cbfdf02b
vamsy <helen123@gmail.com>
2026-03-02 23:10:58 +0530

    data type exercise 1

---
+++ data type-01.js
@@ -0,0 +1,13 @@
+let message = "Login Successful";   // string
+let cartCount = 5;                  // number
+let isVisible = true;               // boolean
+console.log(message);
+console.log(cartCount);
+console.log(isVisible);
+
+console.log(typeof message);
+console.log(typeof cartCount);
+console.log(typeof isVisible);
+
+console.log(typeof undefined); // "undefined"
+console.log(typeof null);      // "object" - What is the type of null? -typeof null returns "object" due to a historical bug in JavaScript.
