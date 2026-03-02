42f7b7685086df079f1f2f6a13f4dde0ca9c5807
vamsy <helen123@gmail.com>
2026-03-03 00:31:55 +0530

    if else ex01

---
+++ If-else01.js
@@ -0,0 +1,39 @@
+// 1
+let score = 60;
+
+if (score > 50) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
+
+
+// 2
+let score = 50;
+
+if (score >= 50) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
+
+// 3
+
+let score = "50";
+
+if (score === 50) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
+
+// 4
+
+let score = "50";
+
+if (score == 50) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
+
