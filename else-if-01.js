3d2ec8612bfd689c897982850f9d110b10b346f0
vamsy <helen123@gmail.com>
2026-03-03 01:06:11 +0530

    else if exercise

---
+++ else-if-01.js
@@ -0,0 +1,21 @@
+// 1
+let score = 75;
+
+if (score >= 90) {
+    console.log("Grade A");
+} else if (score >= 70) {
+    console.log("Grade B");
+} else {
+    console.log("Grade C");
+}
+
+// 2
+let score = 95;
+
+if (score >= 90) {
+    console.log("Grade A");
+}
+if (score >= 70) {
+    console.log("Grade B");
+}
+
