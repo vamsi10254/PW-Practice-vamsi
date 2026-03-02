75329c199f9e8257587b0f1e6cb06c14ef30a853
vamsy <helen123@gmail.com>
2026-03-02 21:10:18 +0530

    Reassignment concept of let variable declartion

---
+++ Var ex.js
@@ -0,0 +1,4 @@
+let age = 25;   // Step 1: create variable and assign 25
+age = 30;       // Step 2: update the value
+console.log(age); // Step 3: print updated value,This concept is called:Re-assignment
+
